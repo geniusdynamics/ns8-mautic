fdd31c4deabed7bb3bfacc9a4b0121f654c778fc
kemboi22 <kemboielvis22@gmail.com>
2024-07-02 23:16:50 +0300

    updated from kickstart to mautic

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -24,8 +24,8 @@
         <cv-tile light>
           <cv-form @submit.prevent="configureModule">
             <cv-text-input
-              :label="$t('settings.kickstart_fqdn')"
-              placeholder="kickstart.example.org"
+              :label="$t('settings.mautic_fqdn')"
+              placeholder="mautic.example.org"
               v-model.trim="host"
               class="mg-bottom"
               :invalid-message="$t(error.host)"

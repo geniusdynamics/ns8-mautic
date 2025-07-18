2656d54ae95e79b7937797f75a9a623866192757
Kemboi Elvis <kemboielvis22@gmail.com>
2025-07-18 13:33:28 +0300

    fix: version

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="mautic"
-mautic_version="6.0-apache"
+mautic_version="6-apache"
 
 # Create a new empty container image
 container=$(buildah from scratch)
